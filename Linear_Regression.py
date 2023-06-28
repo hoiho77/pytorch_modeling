e5458e60aabc3e5733da9f1c22de0b3319d563e9
hoiho77 <hoihoho77@gmail.com>
2023-06-28 10:13:51 +0900

    commit

---
+++ Linear_Regression.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+torch.manual_seed(1)
+
+x_train = torch.FloatTensor([[1], [2], [3]])
+y_train = torch.FloatTensor([[2], [4], [6]])
+
+x_train.shape
+
+# requires_grad=True 학습을 통해 값이 변경되는 변수
+W = torch.zeros(1, requires_grad=True) # 가중치
+b = torch.zeros(1, requires_grad=True) # 편향
+
+
+# 경사하강법 구현하기
+optimizer = optim.SGD([W, b], lr = 0.01) # SGD 경사하강법
+
+n_epochs = 1000
+for epoch in range(n_epochs+1):
+    hypothesis = x_train * W + b
+
+    cost = torch.mean((hypothesis - y_train) ** 2)  # MSE
+
+    # gradient를 0으로 초기화
+    optimizer.zero_grad()
+
+    # 비용함수를 미분하여 gradient 계산
+    cost.backward()
+
+    #W와 b를 업데이트
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        print(f'epoch {epoch}/{n_epochs} W :{W.item()}, b :{b.item()}, Cost{cost.item()}')
--- seq2seq.py
+++ seq2seq.py
@@ -1,2 +1,54 @@
 import pandas as pd
 import numpy as np
+import torch
+import torch.nn as nn
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
+
+import spacy
+import random
+import math
+import time
+
+#from torch.utils.data import DataLoader
+#from torch.nn.utils.rnn import pad_sequence
+
+#from collections import Counter
+#from torchtext.datasets import Multi30k
+#from torchtext.vocab import vocab
+#from torchtext.data import get_tokenizer
+
+SEED = 1234
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+# en_core_seb_sm: 영어 토큰화 전용 모델, de_core_news_sm: 독일어 토큰화 전용 모델
+spacy_en = spacy.load('en_core_web_sm')
+spacy_de = spacy.load('de_core_news_sm')
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+# 토큰화 함수를 만드는데, input 문장의 토큰화 이후에 순서를 뒤집는 작업이 추가(논문 상 input 문장의 순서를 뒤집었을 때, 더 좋은 성능을 보임)
+def tokenize_de(text):
+    return [tok.text for tok in spacy_de.tokenizer(text)][::-1]
+
+# 토큰화된 문장을 파이토치 텐서로 받아주는 Field 함수를 정의함 (독일어-> 영어)
+SRC = Field(tokenize = tokenize_de, init_token = '<sos>', eos_token='<eos>', lower=True) # 독일어(source)
+TRG = Field(tokenize = tokenize_en, init_token = '<sos>', eos_token='<eos>', lower=True) #영어(Target)
+
+# 학습데이터 가져오기
+train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'), fields=(SRC, TRG))
+print(f"Number of training examples: {len(train_data.examples)}")
+print(f"Number of validation examples: {len(valid_data.examples)}")
+print(f"Number of testing examples: {len(test_data.examples)}")
+print(vars(train_data.examples[0]))
+
+train_data, valid_data, test_data = Multi30k.splits(exts = ('.de', '.en'), fields = [('src',SRC, 'trg',TRG)])
+
+train_dataset, valid_dataset, test_dataset = Multi30k.splits(exts=(".de", ".en"), fields=(SRC, TRG))
+
+import torchtext
+print(torchtext.__version__)
