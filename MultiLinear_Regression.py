1f4184276e38880037bc4159283f26d8e9a259b5
hoiho77 <hoihoho77@gmail.com>
2023-06-28 11:02:33 +0900

    commit

--- MultiLinear_Regression.py
+++ MultiLinear_Regression.py
@@ -36,3 +36,40 @@ for epoch in range(nb_epochs + 1):
     print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'.format(
         epoch, nb_epochs, hypothesis.squeeze().detach(), cost.item()
     ))
+
+## nn.Module 로 다중 선형회귀 구현하기
+
+MLR = nn.Linear(3,1)
+list(MLR.parameters()) # 랜덤초기화되어 있음
+# 학습률(learning rate)이 모델의 필요한 크기보다 높을 때, 기울기가 발산
+
+optimizer = torch.optim.SGD(MLR.parameters(), lr=1e-5)
+
+nb_epochs = 2000
+for epoch in range(nb_epochs+1):
+
+    # H(x) 계산
+    prediction = MLR(x_train)
+    # model(x_train)은 model.forward(x_train)와 동일함.
+
+    # cost 계산
+    cost = F.mse_loss(prediction, y_train) # <== 파이토치에서 제공하는 평균 제곱 오차 함수
+
+    # cost로 H(x) 개선하는 부분
+    # gradient를 0으로 초기화
+    optimizer.zero_grad()
+    # 비용 함수를 미분하여 gradient 계산
+    cost.backward()
+    # W와 b를 업데이트
+    optimizer.step()
+
+    if epoch % 100 == 0:
+    # 100번마다 로그 출력
+      print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+          epoch, nb_epochs, cost.item()
+      ))
+
+new_var = torch.FloatTensor([[73,80,75]])
+pred_y = MLR(new_var)
+print(pred_y)
+list(MLR.parameters())
